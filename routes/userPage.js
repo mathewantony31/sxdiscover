4ec97820a2b927b30fc7fd29e0b50b6e92ee166d
Mathew <mathew.antony31@gmail.com>
2019-02-02 21:39:22 -0600

    Added calendar functionality with real data

--- routes/userPage.js
+++ routes/userPage.js
@@ -16,39 +16,50 @@ router.get('/pages/*', function(req, res){
         return res.send({errors: "Error connecting to our database"});
       } else{
         try{
+        // Check to see if current page matches the user's logged in session
+        var requestSession = req.session.id;
+        var userSession = docs[0].uid;
+        var displayName = docs[0].displayName;
+        var public = docs[0].public;
+        var link = "http://sxdiscover.co/pages/"+req.params[0];
 
-          // Check to see if current page matches the user's logged in session
-          var requestSession = req.session.id;
-          var userSession = docs[0].uid;
-          var displayName = docs[0].displayName;
-          var public = docs[0].public;
-          var link = "http://sxdiscover.co/pages/"+req.params[0];
+        // Check to see if page is set to public or private, or if user visiting their own page
 
-                 // Check to see if page is set to public or private, or if user visiting their own page
+        var results = Band.fetchBandInfo(docs[0].rawBandsFromSpotify, function(result){
 
-                 var results = Band.fetchBandInfo(docs[0].rawBandsFromSpotify, function(result){
+          result.sort(function(a,b){
 
-                  var r = JSON.stringify(result);
-
-                  if(public==true){
-                    if(requestSession==userSession){
-                      // If user is visiting their own page, show the delete button
-                      res.render('userPage', {bands:r, canDelete:'y', public:'y', status:"public", link:link});
-                    } else {
-                      res.render('userPage', {bands:r, canDelete:'n', public:'y', status:"public", link:link});
-                    }
-                  } else if(requestSession==userSession){
-                    res.render('userPage', {bands:r, canDelete:'y', public:'n', status:"private"});
-                  } else {
-                    return res.render('privateUser');
-                  }
-                });
-               } catch (e){
-                console.log("Error is " + e);
-                return res.send({errors: "User page doesn't exist"});
-              }
+            if(a.time=='TBD'){
+              a.time = '00:00'
             }
-          });
+            if(b.time=='TBD'){
+              b.time = '00:00'
+            }
+            return Date.parse(a.date+" 2018 "+a.time)-Date.parse(b.date+" 2018 "+b.time);
+          })
+
+          var r = parseShowData(result);
+          console.log(r);
+
+          if(public==true){
+            if(requestSession==userSession){
+            // If user is visiting their own page, show the delete button
+            res.render('userPage', {shows:r, canDelete:'y', public:'y', status:"public", link:link});
+          } else {
+            res.render('userPage', {shows:r, canDelete:'n', public:'y', status:"public", link:link});
+          }
+        } else if(requestSession==userSession){
+          res.render('userPage', {shows:r, canDelete:'y', public:'n', status:"private"});
+        } else {
+          return res.render('privateUser');
+        }
+      });
+      } catch (e){
+        console.log("Error is " + e);
+        return res.send({errors: "User page doesn't exist"});
+      }
+    }
+  });
   } catch (e){
     res.send(404);
   }
@@ -92,6 +103,71 @@ function fetchBandSummary(user, callback){
       }
     }
   }); 
+}
+
+function parseShowData(shows){
+
+  var dateList = [1];
+
+  for(var i=0;i<shows.length;i++){
+
+    var showDate = Date.parse(shows[i].date).getDate();
+    var showInfo = {
+      name:shows[i].name,
+      time:shows[i].time,
+      link:shows[i].link,
+      venue:shows[i].venue,
+      price:shows[i].price
+    }
+
+    // For each element in dateList, check if the element.date equals an integer
+    for(var j=0;j<dateList.length;j++){
+      if(dateList[j].date==showDate){
+        dateList[j].shows.push(showInfo)
+        break;
+      }
+
+      // If we're at the last element and still haven't found a date match, create a new date in dateList
+      if(j==dateList.length-1){
+        dateList.push({
+          date:showDate,
+          shows:[showInfo]
+        })
+      }
+    }
+  }
+
+  // Remove first element
+  dateList.shift();
+
+  return dateList
+  // Create an array of objects, where each item is a day with an array of show data, e.g. [{'2019-03-12':[{show1},{show2},{show3}]}]. Each show contains the properties defined in the Band schema
+}
+
+// Function that returns the correct suffix for a given date, e.g. if you input 22, it will return "nd" to form "22nd". The approach here is to extract the second digit from the date (e.g. if you input 23, it'll just look at 3), and return a string based on the value of the second digit: 1 = "st", 2 = "nd", 3 = "rd", everything else = "th".
+
+function getDateSuffix(date){
+// Pull the second digit
+var secondDigit = date % 10
+
+// Special cases: 11, 12 and 13 are treated as "th"
+if(date==11 || date==12 || date==13){
+  return "th"
+}
+  // 1st, 2nd, 3rd, 4th, 21st, 22nd, 23rd
+  switch(secondDigit){
+    case 1:
+    return "st"
+    break;
+    case 2:
+    return "nd"
+    break;
+    case 3:
+    return "rd"
+    break;
+    default:
+    return "th"
+  }
 }
 
 module.exports = router;
